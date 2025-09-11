a61883525d08350ab5d5412be829ab12d991c4ad
Hicham Elfatihi <helfatih@c1r4p1.1337.ma>
2025-09-11 17:54:42 +0100

    complete the ex04

---
+++ module01/ex04/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+
+void  replace_function(char **arr, std::ifstream &in_file, std::ofstream &out_file)
+{
+  std::string line;
+  std::string s1 = arr[2];
+  std::string s2 = arr[3];
+  int start_pos = 0;
+  
+  while (getline(in_file, line))
+  {
+    while ((start_pos = line.find(s1, 0)) != std::string::npos)
+    {
+      line = line.substr(0, start_pos) + s2 + line.substr(start_pos + s1.length());
+    }
+    out_file << line;
+    if (!in_file.eof())
+      break;
+  }
+  out_file << std::endl;
+}
+
+void  open_and_read(char **arr)
+{
+  std::ifstream in_file;
+  std::ofstream out_file;
+
+  in_file.open(arr[1]);
+  if (!in_file.is_open())
+  {
+    std::cerr << "the in_file maybe not open !\n";
+    return ;
+  }
+
+  out_file.open("out_file.txt");
+  if (!out_file.is_open())
+  {
+    std::cerr << "the out_file maybe not open !\n";
+    return ;
+  }
+
+  replace_function(arr, in_file, out_file);
+
+  std::cout << "we finish !!" << std::endl;
+}
+
+int main (int argc, char *argv[])
+{
+  if (argc != 4)
+  {
+    std::cerr << "Oops you don't pass enough argument !" << std::endl;
+    return (1);
+  }
+  open_and_read(argv);
+  return 0;
+}
